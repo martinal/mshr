0dbb56046cdf3b6de4b027e1df3d4a1306bc4953
Benjamin Kehlet <benjamik@simula.no>
2014-01-31 12:55:47 +0100

    Remove some whitespace.

--- include/dolfincsg.h
+++ include/dolfincsg.h
@@ -1,13 +1,9 @@
-#include <dolfincsg/CSGCGALDomain2D.h>
+#include <dolfincsg/CSGMeshGenerator.h>
+#include <dolfincsg/CSGOperators.h>
+#include <dolfincsg/CSGPrimitives2D.h>
+#include <dolfincsg/CSGPrimitives3D.h>
 #include <dolfincsg/CSGCGALMeshGenerator2D.h>
 #include <dolfincsg/CSGCGALMeshGenerator3D.h>
 #include <dolfincsg/CSGGeometries3D.h>
 #include <dolfincsg/CSGGeometry.h>
-#include <dolfincsg/CSGMeshGenerator.h>
-#include <dolfincsg/CSGOperators.h>
 #include <dolfincsg/CSGPrimitive.h>
-#include <dolfincsg/CSGPrimitives2D.h>
-#include <dolfincsg/CSGPrimitives3D.h>
- 
- 
- 
