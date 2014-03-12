10a94ef7bc405aa383da2e177d97fcfab9b1caf9
Benjamin Kehlet <benjamik@simula.no>
2014-03-12 17:26:27 +0100

    Put command line parsing in a separate function and improve user message.

--- app/mshr-surfacefile.cpp
+++ app/mshr-surfacefile.cpp
@@ -29,44 +29,88 @@
 namespace po = boost::program_options;
 
 // This program reads in a surface from file and generates mesh
-int main(int argc, char** argv)
+
+
+namespace
+{
+void print_mesh_statistics(const dolfin::Mesh& m)
+{
+  {
+    dolfinSquareMesh x(2,2);
+  }
+  std::cout << "Dolfin mesh of topological dimension " << m.topology().dim() << std::endl;
+  std::cout << "  " << m.num_vertices() << " vertices" << std::endl;
+  std::cout << "  " << m.num_cells() << " cells" << std::endl;
+
+  const std::pair<double, double> radii_ratio = dolfin::MeshQuality::radius_ratio_min_max(m);
+  std::cout << "Minimum cell radii ratio: " << radii_ratio.first  << std::endl;
+  std::cout << "Maximum cell radii ratio: " << radii_ratio.second << std::endl;
+}
+//-----------------------------------------------------------------------------
+// Define options and parse command line
+void handle_commandline(int argc, char** argv, po::variables_map &vm)
 {
   // Command line options
-  po::options_description desc("Generate mesh from surface file");
-  desc.add_options()
-    ("infile",     po::value<std::string>(), "Filename of infile")
-    ("outfile",    po::value<std::string>()->default_value("outmesh.xml"), "Filename of outfile")
-    ("resolution", po::value<double>()->default_value(5.0), "Resolution of result mesh")
-    ("help",                                                "write help message");
+  po::options_description visible("Generate mesh from surface file");
+  visible.add_options()
+    ("outfile,o",    po::value<std::string>(), "Filename of generated Dolfin mesh")
+    ("resolution,r", po::value<double>()->default_value(15.0), "Resolution of result mesh")
+    ("stats,s", "Write some statistics of the mesh to stdout")
+    ("help,h",   "write help message");
+
+  // Options not shown to the user
+  po::options_description hidden("Hidden options");
+  hidden.add_options()
+    ("input-file", po::value<std::string>(), "Input file")
+    ;
+
+  po::options_description cmdline("Generate mesh from surface file");
+  cmdline.add(visible).add(hidden);
 
   //Command line positional arguments
-  po::positional_options_description positional_options; 
-  positional_options.add("infile", 1); 
+  po::positional_options_description p;
+  p.add("input-file", 1);
 
   // parse command line
-  po::variables_map vm;
   po::store(po::command_line_parser(argc, argv)
-            .options(desc)
-            .positional(positional_options).run(), vm);
+            .options(cmdline)
+            .positional(p).run(), vm);
 
   // Print help message if requested 
   // (before notify to avoid error messages if only --help is given)
-  if (vm.count("help")) { std::cout << desc << std::endl;  exit(0); }
+  if (vm.count("help"))
+  {
+    std::cout << visible << std::endl;
+    exit(EXIT_SUCCESS);
+  }
 
   po::notify(vm);
 
-  std::cout << "Infile: " << vm["infile"].as<std::string>() << std::endl;
-  mshr::Surface3D surf(vm["infile"].as<std::string>());
+}
+} //end anonymous namespace
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  po::variables_map vm;
+  handle_commandline(argc, argv, vm);
 
+  // Read the infile
+  mshr::Surface3D surf(vm["input-file"].as<std::string>());
+
+  // Generate the mesh
   dolfin::Mesh m;
+  //mshr::CSGMeshGenerator::generate(m, surf, vm["resolution"].as<double>());
 
-  mshr::CSGMeshGenerator::generate(m, surf, vm["resolution"].as<double>());
-  
-  dolfin::File f(vm["outfile"].as<std::string>());
-  f << m;
+  // Output mesh if requested
+  if (vm.count("outfile"))
+  {
+    dolfin::File f(vm["outfile"].as<std::string>());
+    f << m;
+  }
     
+  if (vm.count("stats"))
+    print_mesh_statistics(m);
 
-  std::cout << "Done" << std::endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
