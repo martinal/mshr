5c6503ca2051de6264b437012c7c082036aa9239
Benjamin Kehlet <benjamik@simula.no>
2014-06-24 10:27:16 +0200

    Revert mistake.

--- 3rdparty/CGAL-4.4/examples/Snap_rounding_2/snap_rounding.cpp
+++ 3rdparty/CGAL-4.4/examples/Snap_rounding_2/snap_rounding.cpp
@@ -6,7 +6,7 @@
 
 typedef CGAL::Quotient<CGAL::MP_Float>           Number_type;
 typedef CGAL::Cartesian<Number_type>             Kernel;
-typedef CGAL::Snap_rounding_traits_2<Kernel>     Trait;
+typedef CGAL::Snap_rounding_traits_2<Kernel>     Traits;
 typedef Kernel::Segment_2                        Segment_2;
 typedef Kernel::Point_2                          Point_2;
 typedef std::list<Segment_2>                     Segment_list_2;
